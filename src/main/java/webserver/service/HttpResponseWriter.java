bd9759c24757522d90595ad3586dce7e0377046f
Oleksandr Haleta <alexhaleta99@gmail.com>
2021-11-04 23:05:39 +0200

    added test HttpResponseWriterTest

--- src/main/java/webserver/service/HttpResponseWriter.java
+++ src/main/java/webserver/service/HttpResponseWriter.java
@@ -8,18 +8,27 @@ public class HttpResponseWriter implements ResponseWriter {
 
     @Override
     public void writeSuccessResponse(OutputStream outputStream, String content) {
+        if (outputStream == null) {
+            throw new NullPointerException("The outputStream cannot be null in 200method.");
+        }
+        if (content == null) {
+            throw  new NullPointerException("The content cannot be null.");
+        }
         try {
             outputStream.write("HTTP/1.1 200 OK\n".getBytes(StandardCharsets.UTF_8));
             outputStream.write("\n".getBytes());
             outputStream.write(content.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Unable to send content", e);
         }
 
     }
 
     @Override
     public void writeNotFoundResponse(OutputStream outputStream) {
+        if (outputStream == null) {
+            throw new NullPointerException("The outputStream cannot be null in 404method.");
+        }
         try {
             outputStream.write("HTTP/1.1 404 Not Found\n".getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
---
+++ src/test/java/webserver/service/HttpResponseWriterTest.java
@@ -0,0 +1,50 @@
+package webserver.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Oleksandr Haleta
+ * 2021
+ */
+class HttpResponseWriterTest {
+    ResponseWriter writer = new HttpResponseWriter();
+    OutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = Mockito.mock(OutputStream.class);
+    }
+
+    @Test
+    void shouldThrowNPExceptionCauseOutputStreamNullWriteSuccessResponse() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            writer.writeSuccessResponse(null, "content");
+        });
+        assertTrue(exception.getMessage().contains("The outputStream cannot be null in 200method."));
+    }
+
+    @Test
+    void shouldThrowNPExceptionCauseContentNullWriteSuccessResponse() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            writer.writeSuccessResponse(outputStream, null);
+        });
+        assertTrue(exception.getMessage().contains("The content cannot be null."));
+    }
+
+    @Test
+    void shouldThrowNPExceptionCauseOutputStreamNullWriteNotFoundResponse() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            writer.writeNotFoundResponse(null);
+        });
+        assertTrue(exception.getMessage().contains("The outputStream cannot be null in 404method."));
+    }
+}
